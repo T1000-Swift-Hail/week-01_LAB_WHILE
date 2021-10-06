42f8219d714ee066239f4186f07803d834077c31
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-06 14:46:49 +0300

    Solv Bonus

--- while.playground/Contents.swift
+++ while.playground/Contents.swift
@@ -3,10 +3,17 @@ import UIKit
 var fruits = ["apple", "orange", "cherry"]
      
 
-while !fruits.isEmpty {
+//while !fruits.isEmpty {
+//    print(fruits.first!)
+//    fruits.removeFirst()
+//
+//}
+
+while (fruits.count != 0) {
     print(fruits.first!)
     fruits.removeFirst()
     
 }
 
 print(fruits) // this is Empty Array now
+
