9b9fa3ab60529039164c8fb7b09a8e64e8ce4047
user <user@MacBook-Air-.local>
2021-10-06 14:50:31 +0300

    done homework

---
+++ fruits.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Darwin
+
+var fruits = ["apple", "orange", "cherry"]
+while !fruits .isEmpty {
+    
+    fruits.first
+    print(fruits)
+    fruits.removeFirst()
+    
+    
+    
+    
+    
+}
