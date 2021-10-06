5d046daea9814002e47e2afe54fefa9c64352f8a
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-06 15:35:20 +0300

    HW it's Done

---
+++ HWlabwhile.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Darwin
+
+var fruits = ["apple","orange","cherry"]
+
+
+
+
+while !fruits.isEmpty {
+   
+    print(fruits.first)
+    
+    fruits.removeFirst()
+    
+    
+    
+}
