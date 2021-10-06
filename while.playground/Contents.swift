3733bd5926a2b30de044a119554f13d08c58f8da
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-06 14:32:22 +0300

    First Solv

---
+++ while.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var fruits = ["apple", "orange", "cherry"]
+     
+
+while !fruits.isEmpty {
+    print(fruits.first!)
+    fruits.removeFirst()
+    
+}
+
+print(fruits) // this is Empty Array now
