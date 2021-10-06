be69aef37c7f2b28e7b5c3748e17bbc8a62009f9
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-06 14:38:06 +0300

    Whilehomework

---
+++ fruits.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Darwin
+
+var fruits = ["apple","orange","cherry"]
+while !fruits.isEmpty {
+    print(fruits.first)
+    fruits.removeFirst() /*I dont
+                          like eat
+                          apple*/
+}
