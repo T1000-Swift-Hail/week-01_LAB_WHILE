cfca80e7308d08601a241e7aa0faebdcd7c3f879
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-06 15:35:11 +0300

    done lab while

--- fruits.playground/Contents.swift
+++ fruits.playground/Contents.swift
@@ -1,10 +1,12 @@
 import UIKit
-import Darwin
 
 var fruits = ["apple","orange","cherry"]
+
 while !fruits.isEmpty {
-    print(fruits.first)
-    fruits.removeFirst() /*I dont
-                          like eat
-                          apple*/
+    
+    print(fruits.first!)
+    
+    fruits.removeFirst()
+    
+    /* I dont like eat apple */
 }
